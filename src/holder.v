dab5b604a8b3b27baea25fc89ef5800375ec3b8a
IchBinEineKartoffel <stephen.lonergan@hotmail.com>
2016-04-11 13:19:12 +1000

    Update holder.v

--- src/holder.v
+++ src/holder.v
@@ -9,23 +9,32 @@ module holder (
 	output wire clk);
 
 	reg [8:0] total = 9'b000000000;
-	reg [8:0] count = 9'b000000000;
 	reg fin = 1'b0;
-	reg write_deb;
+	wire write_deb;
+	reg deb = 1'b0;
+	reg [7:0] count = 9'b000000000;
 	
 	clockdiv #(15,5207) clockdiv(.sysclk(sysclk),.pulse(clk));
 	
 	debouncer debouncer_write(.sysclk(sysclk),.btn(write),.btn_deb(write_deb));
-    
+	
+	always @(posedge sysclk) begin
+		if (write_deb == 1) deb <= 1;
+		if ((write == 0) && (fin == 1)) deb <= 0;
+	end
+
     always @(posedge clk) begin
-		if ((write_deb == 0)&&(out == 0)) fin = 0;
+		if ((deb == 0)&&(out == 0)) fin <= 0;
 	 	if ((out == 1)&&(count < total)&&(fin == 0))
 			begin
 				count <= count + 1;
 			end
-		else if ((write_deb == 1) && (out == 0) && (fin == 0) && ((sw1==1)||(sw2==1)||(sw3==1)||(sw4==1)))
+		else if ((deb == 1) && (out == 0) && (fin == 0) && ((sw1==1)||(sw2==1)||(sw3==1)||(sw4==1)))
 			begin
-				total <= total + (sw1 * 9'b001011000) + (sw2 * 9'b001010000) + (sw3 *  9'b000111000) + (sw4 * 9'b000100000);
+				if (sw1 == 1) total <= total + 143;
+				if (sw2 == 1) total <= total + 130;
+				if (sw3 == 1) total <= total + 91;
+				if (sw4 == 1) total <= total + 52;
 				out <= 1;
 			end
 		else if ((out == 1)&&(count == total))					
