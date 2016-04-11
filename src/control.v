ff6d1ab3e336c9ec3e4d743df8869e00d1d74ee1
IchBinEineKartoffel <stephen.lonergan@hotmail.com>
2016-04-11 13:20:35 +1000

    Update control.v

--- src/control.v
+++ src/control.v
@@ -7,8 +7,8 @@ module control (input wire sysclk, input wire write, input wire sw1, input wire 
 	wire [7:0] d2;
 	wire [7:0] d3;
 	wire [7:0] d4;
-	wire [2:0] count13;
 	wire [7:0] count;
+	wire clk;
 	
     // rom instances
     rom1 rom1(.data(d1),.addr(addr));
@@ -16,10 +16,13 @@ module control (input wire sysclk, input wire write, input wire sw1, input wire 
     rom3 rom3(.data(d3),.addr(addr));
     rom4 rom4(.data(d4),.addr(addr));
     
-	holder holder(.sysclk(sysclk), .write(write), .sw1(sw1), .sw2(sw2), .sw3(sw3), .sw4(sw4), .out(holderOut), .);
-	splitter splitter(.sw1(sw1), .sw2(sw2), .sw3(sw3), .sw4(sw4), .holder(holderOut), .rom1(d1), .rom2(d2), .rom3(d3), .rom4(d4), .count13(count13), .currentData(currentData), .count(count));
+	holder holder(.sysclk(sysclk), .write(write), .sw1(sw1), .sw2(sw2), .sw3(sw3), .sw4(sw4), .out(holderOut), .clk(clk));
+	
+	splitter splitter(.clk(clk), .sw1(sw1), .sw2(sw2), .sw3(sw3), .sw4(sw4), .holder(holderOut), .rom1(d1), .rom2(d2), .rom3(d3), .rom4(d4), .currentData(currentData), .count(count));
+	
 	addr addr1(.clk(clk), .count(count), .addr(addr));
-	cereal cereal(.sysclk(sysclk),.data(currentData),.start(start),.cereal(out_fin),.status(),.pulse());
+	
+	cereal cereal(.sysclk(sysclk),.data(currentData),.start(holderOut),.cereal(out_fin),.status(),.pulse());
 	 
 
 endmodule
