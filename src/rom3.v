b901c9d78cfc334c5a2500b0a0e62974dfdbc92b
IchBinEineKartoffel <stephen.lonergan@hotmail.com>
2016-04-12 12:06:30 +1000

    Update rom3.v

--- src/rom3.v
+++ src/rom3.v
@@ -8,6 +8,7 @@ module rom3 (input wire [3:0] addr, output reg [7:0] data);
             4'b0100: data = 8'b01000101; // E
             4'b0101: data = 8'b01001110; // N
             4'b0110: data = 8'b01010100; // T
+		4'b0111: data = 8'b00100000;
             default: data = 8'b00000000;
         endcase
     end
