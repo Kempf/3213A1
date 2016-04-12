d640a7323a8e39be5ca5793b2cb77d49414e3175
IchBinEineKartoffel <stephen.lonergan@hotmail.com>
2016-04-12 12:07:07 +1000

    Update rom4.v

--- src/rom4.v
+++ src/rom4.v
@@ -5,6 +5,7 @@ module rom4 (input wire [3:0] addr, output reg [7:0] data);
             4'b0001: data = 8'b01010000; // P
             4'b0010: data = 8'b01000111; // G
             4'b0011: data = 8'b01000001; // A
+				4'b0100: data = 8'b00100000;
             default: data = 8'b00000000;
         endcase
     end
