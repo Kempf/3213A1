1a084cca04eb4a796ad70a524b8463e7f605a898
IchBinEineKartoffel <stephen.lonergan@hotmail.com>
2016-04-18 20:37:22 +1000

    Update control_f3.v

--- src/control_f3.v
+++ src/control_f3.v
@@ -8,7 +8,7 @@ module control_f3 (input wire sysclk, input wire reset, input wire send, input w
 	wire write;
 
    ram ram(.sysclk(sysclk), .write(write), .addr(addr), .data_in(data_in), .reset(reset), .data_out(data_out));    
-	dataControlModule dataControlModule(,clk(clk), .sysclk(sysclk), .serialIn(serialIn), .reset(reset), .send(send), .write(write), .addr(addr), .serialToRAM(serialToRAM), .start_latch(send_latch));	
-	cereal cereal(.sysclk(sysclk),.data(data_out),.start(send_latch),.cereal(out_fin),.status(),.pulse(), .data_latch()); // Finish hooking this up
+	dataControlModule dataControlModule(.clk(clk), .sysclk(sysclk), .serialIn(serialIn), .reset(reset), .send(send), .write(write), .addr(addr), .serialToRAM(serialToRAM), .send_latch(send_latch));	
+	cereal cereal(.sysclk(sysclk),.data(data_out),.start(send_latch),.cereal(out_fin),.status(),.pulse()); // Finish hooking this up
 	 
 endmodule
