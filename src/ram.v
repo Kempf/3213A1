31af7ca6317a80c96e679ff77f49122ec588d236
IchBinEineKartoffel <stephen.lonergan@hotmail.com>
2016-04-18 20:36:51 +1000

    Update ram.v

--- src/ram.v
+++ src/ram.v
@@ -6,15 +6,15 @@ input wire reset,
 output reg [15:0] data_out);
 
 reg [15:0] mem [255:0];
-reg i = 0;
-reg reset_deb;
+integer i;
+wire reset_deb;
 
 debouncer debouncer_reset(.sysclk(sysclk),.btn(write),.btn_deb(reset_deb));
 
 always @(posedge sysclk) begin
 	if (write) mem[addr] <= data_in;
 	data_out<=mem[addr];
-	if (reset) begin
+	if (reset_deb) begin
 		for (i = 0; i < 256; i = i+1) begin
 			mem[i] <= 0;
 		end
