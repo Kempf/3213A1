1251a39eab9440daada411ae2371529362e0171e
IchBinEineKartoffel <stephen.lonergan@hotmail.com>
2016-04-20 00:07:06 +1000

    Update holder.v

--- src/holder.v
+++ src/holder.v
@@ -13,17 +13,13 @@ module holder (
 	output reg sw4_latch,
 	output reg auto_latch,
 	output reg out,
-	output wire clk,
-	output reg outTrig);
+	output wire clk);
 
 	reg [9:0] total;
 	wire write_deb;
 	reg deb;
 	reg [9:0] count;
-	reg latch;
 	wire auto_deb;
-	reg [9:0] cycle_len;
-	reg outTrigTrig;
 		
 	clockdiv #(15,5207) clockdiv(.sysclk(sysclk),.pulse(clk));
 	
@@ -35,59 +31,53 @@ module holder (
 	always @(posedge sysclk) begin
 		if (reset) begin
 			deb <= 0;
+			auto_latch <= 0;
 		end
 		else begin
 			if (write_deb == 1) deb <= 1;							//Locks a value at sysclk freq
-			if ((count > 0) && (count == total)) deb <= 0;
-		end
-	end
+			if (((count > 0) && (count == total))||((sw1 ==0)&&(sw2==0)&&(sw3==0)&&(sw4==0))) deb <= 0;
 
-	
-	always @(posedge auto_deb) begin
-	if (out == 0)
-		begin
-			if (auto_latch != 1) 
-				begin
-					auto_latch <= 1;
-					//cycle_len <= (sw1 * 131) + (sw2 * 120) + (sw3 * 87) + (sw4 * 54);
-					if (sw1 == 1) sw1_latch <= 1;
-					if (sw2 == 1) sw2_latch <= 1;
-					if (sw3 == 1) sw3_latch <= 1;
-					if (sw4 == 1) sw4_latch <= 1;
+			if (auto_deb)begin 
+				if (out == 0)
+					begin
+						if (auto_latch != 1) 
+							begin
+								auto_latch <= 1;
+								if (sw1 == 1) sw1_latch <= 1;
+								if (sw2 == 1) sw2_latch <= 1;
+								if (sw3 == 1) sw3_latch <= 1;
+								if (sw4 == 1) sw4_latch <= 1;
+							end
+						else 
+							begin
+								auto_latch <= 0;
+								sw1_latch <= 0;
+								sw2_latch <= 0;
+								sw3_latch <= 0;
+								sw4_latch <= 0;
+							end
+					end
+				else 
+					begin
+						auto_latch <= 0;
+						sw1_latch <= 0;
+						sw2_latch <= 0;
+						sw3_latch <= 0;
+						sw4_latch <= 0;
+					end	
 				end
-			else 
-				begin
-					auto_latch <= 0;
-					//cycle_len <= 0;
-					sw1_latch <= 0;
-					sw2_latch <= 0;
-					sw3_latch <= 0;
-					sw4_latch <= 0;
-				end
+			end
 		end
-	else 
-		begin
-			auto_latch <= 0;
-			//cycle_len <= 0;
-			sw1_latch <= 0;
-			sw2_latch <= 0;
-			sw3_latch <= 0;
-			sw4_latch <= 0;
-		end		
-	end
+
 	
     always @(posedge clk) begin
 		if (reset) begin
 			out <= 0;
-			outTrigTrig <= 0;
-			cycle_len <= 0;
-			latch <= 0;
 			count <= 9'b000000000;
 			total <= 9'b000000000;	
 		end
 		 else begin
 		 	if ((sw1 ==0) && (sw2 == 0) && (sw3 == 0) && (sw4 == 0)) total <= 0;
-			outTrig <= (out);
 			begin		
 				if ((out == 1)&&(count < total))
 					begin
@@ -97,21 +87,19 @@ module holder (
 					begin
 						out <= 0;
 						count <= 9'b000000000;
-						latch <= 0;
 					end
 				else if (deb == 1 && ((sw1==1)||(sw2==1)||(sw3==1)||(sw4==1))) 
 					begin
 						out <= 1;	
-						if (sw1) total <= total + 155;
-						if (sw2) total <= total + 142;
-						if (sw3) total <= total + 116;
-						if (sw4) total <= total + 77;						
+						if (sw1) total <= total + 156;
+						if (sw2) total <= total + 143;
+						if (sw3) total <= total + 104;
+						if (sw4) total <= total + 66;
 					end
 				else
 					begin
 						out <= 0;
 						count <= 9'b000000000;
-						latch <= 0;
 					end
 				end
 			end
