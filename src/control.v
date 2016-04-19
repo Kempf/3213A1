034a09ff928d5a3aa5c85a55d45de88cd82038d8
IchBinEineKartoffel <stephen.lonergan@hotmail.com>
2016-04-20 00:06:46 +1000

    Update control.v

--- src/control.v
+++ src/control.v
@@ -8,7 +8,7 @@ module control (input wire sysclk, input wire write, input wire reset, input wir
 	wire [7:0] d2;
 	wire [7:0] d3;
 	wire [7:0] d4;
-	wire [7:0] count;
+	wire [9:0] count;
 	wire clk;
 	wire sw1_latch;
 	wire sw2_latch;
@@ -16,9 +16,6 @@ module control (input wire sysclk, input wire write, input wire reset, input wir
 	wire sw4_latch;
 	wire trig;
 	assign out_debug = out_fin;
-/*	wire [7:0] data;
-	assign data = 8'b01010101;*/
-	wire [3:0] count11;
 	
     // rom instances
     rom1 rom1(.data(d1),.addr(addr));
@@ -28,7 +25,7 @@ module control (input wire sysclk, input wire write, input wire reset, input wir
     
 	holder holder(.sysclk(sysclk), .write(write), .auto(auto), .sw1(sw1), .sw2(sw2), .sw3(sw3), .sw4(sw4), .sw1_latch(sw1_latch), .sw2_latch(sw2_latch), .reset(reset), .sw3_latch(sw3_latch), .sw4_latch(sw4_latch), .out(holderOut), .auto_latch(holderAutoLatch), .clk(clk));
 	
-	splitter splitter(.count11(count11), .reset(reset), .sysclk(sysclk), .outTrig(trig), .clk(clk), .sw1(sw1), .sw2(sw2), .sw3(sw3), .sw4(sw4), .sw1_latch(sw1_latch), .sw2_latch(sw2_latch), .sw3_latch(sw3_latch), .sw4_latch(sw4_latch), .holder(holderOut), .auto_latch(holderAutoLatch), .rom1(d1), .rom2(d2), .rom3(d3), .rom4(d4), .currentData(currentData), .count(count));
+	splitter splitter(.reset(reset), .sysclk(sysclk), .outTrig(trig), .clk(clk), .sw1(sw1), .sw2(sw2), .sw3(sw3), .sw4(sw4), .sw1_latch(sw1_latch), .sw2_latch(sw2_latch), .sw3_latch(sw3_latch), .sw4_latch(sw4_latch), .holder(holderOut), .auto_latch(holderAutoLatch), .rom1(d1), .rom2(d2), .rom3(d3), .rom4(d4), .currentData(currentData), .count(count));
 	
 	addr addr1(.clk(clk), .count(count), .addr(addr));
 	
