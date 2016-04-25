4ea9211c98a404d84fc8b543586dcd8a577727d6
IchBinEineKartoffel <stephen.lonergan@hotmail.com>
2016-04-25 21:37:49 +1000

    Update ram.v

--- src/f3_ausf_a/ram.v
+++ src/f3_ausf_a/ram.v
@@ -2,21 +2,13 @@ module ram(input wire sysclk,
 input wire write,
 input wire [7:0] addr,
 input wire [15:0] data_in,
-input wire reset,
 output reg [15:0] data_out);
 
 reg [15:0] mem [255:0];
-integer i;
 
 always @(posedge sysclk) begin
 	if (write) mem[addr] <= data_in;
 	data_out<=mem[addr];
-	
-	if (reset) begin
-		for (i = 0; i < 256; i = i+1) begin
-			mem[i] <= 16'b0000000000000000;
-		end
-	end
 end
 
 endmodule
