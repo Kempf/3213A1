b1d635194c46058ef7288fc9bdcbe178bb512ab7
Paul Apelt <lt.kempf@gmail.com>
2016-04-26 16:45:39 +1000

    Delete s_mux.v

--- src/f4_ausf_a/s_mux.v
+++
@@ -1,9 +0,0 @@
-module s_mux( input wire a1, input wire a2, input wire a3, input wire o1, input wire o2, input wire o3, output reg o);
-
-always @(*) begin
-	if(a1) o = o1;
-	else if (a2) o = o2;
-	else o = o3;
-end
-
-endmodule
