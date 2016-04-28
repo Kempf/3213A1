03e1186949ab4b0027f5ecf4de3a9f414b373113
Paul Apelt <lt.kempf@gmail.com>
2016-04-28 11:45:01 +1000

    Update and rename src/f4_ausf_a/ausf_b/mux_2.v to src/f4_ausf_b/l_mux.v

--- src/f4_ausf_a/ausf_b/mux_2.v
+++
@@ -1,8 +0,0 @@
-module mux_2( input wire writeOut, input wire underLimit, input wire serialIn, input wire select, output reg out);
-
-always @(*) begin
-	if(select && underLimit) out = serialIn;
-	else out = writeOut;
-end
-
-endmodule
